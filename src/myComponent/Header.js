4a847f17bc71ded5a8e7be1be4a36210e6754c8c
Rabby Khan <rabbikhan365@gmail.com>
2023-01-10 15:31:09 +0600

    Position fixed Navbar

--- src/myComponent/Header.js
+++ src/myComponent/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar bg-dark" data-bs-theme="dark">
+    <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary navbar bg-dark" data-bs-theme="dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">React Bootstrap</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
